c27e52926f92c140c4759e46f4ebd6a4e1444405
Joakim Frogner <joakim@inligo.no>
2019-02-13 13:22:13 +0100

    sleep

--- code/python/1d_conv_4h.py
+++ code/python/1d_conv_4h.py
@@ -166,7 +166,7 @@ print(model.summary())
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-BATCH_SIZE = 400
+BATCH_SIZE = 200
 EPOCHS = 40
 
 history = model.fit(X_train,

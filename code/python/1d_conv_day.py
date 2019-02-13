d0b0d208e9e811ff6f8131ca880e5196167cfb0b
Joakim Frogner <joakim@inligo.no>
2019-02-13 14:22:44 +0100

    attempt better code

--- code/python/1d_conv_day.py
+++ code/python/1d_conv_day.py
@@ -1,190 +1,22 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-
-import os
-import random
-import time
-import datetime
-
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-
-from IPython.display import display
-
-from matplotlib import pyplot as plt
-
-from tensorflow import keras
-from tensorflow.keras import backend as K
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Dropout, Flatten, Reshape, GlobalAveragePooling1D
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
-from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
-
-pd.options.mode.chained_assignment = None
-
-PROJECT_DIR = '../'
-DATASET_DIR = 'datasets'
-SAVE_DIR = 'data'
-
-try:
-    os.chdir(PROJECT_DIR)
-except:
-    print('No such directory')
-    pass
-
-CATEGORIES = ['CONDITION', 'CONTROL']
-LABELS = ['normal', 'bipolar']
-
-def make_confusion_matrix(validations, predictions, print_stdout=False, save=True):
-    matrix = confusion_matrix(validations, predictions)
-    plt.figure(figsize=(6, 4))
-    sns.heatmap(matrix,
-                cmap="coolwarm",
-                linecolor='white',
-                linewidths=1,
-                xticklabels=LABELS,
-                yticklabels=LABELS,
-                annot=True,
-                fmt="d")
-    plt.title("Confusion Matrix")
-    plt.ylabel("True Label")
-    plt.xlabel("Predicted Label")
-
-    if save:
-        plt.savefig('img/day/confusion_matrix.png')
-
-    if print_stdout:
-        print('Confusion matrix:\n', matrix)
-
-def average_str(string):
-    if(type(string) == str):
-        num1, num2 = string.split('-')
-        num1 = int(num1)
-        num2 = int(num2)
-
-        return (num1 + num2) // 2
-
-    else:
-        return string
-
-def time_between(now, start, end):
-    if start <= end:
-        return start <= now < end
-    else:
-        return start <= now or now < end
-
-def extract_time_of_day(timestamp):
-    ts = time.strptime(timestamp.split(' ')[1], '%H:%M:%S')
-
-    for label in part_of_days.keys():
-        times = part_of_days[label]
-        start = time.strptime(times[0], '%H:%M:%S')
-        end = time.strptime(times[1], '%H:%M:%S')
-
-        if time_between(ts, start, end):
-            return label
-
-    return None
-
-def is_daytime(timestamp):
-    return time_between(time.strptime(timestamp.split(' ')[1], '%H:%M:%S'),
-                        time.strptime('06:00:00', '%H:%M:%S'),
-                        time.strptime('21:00:00', '%H:%M:%S'))
-
-def feature_normalize(dataset):
-    mu = np.mean(dataset, axis=0)
-    sigma = np.std(dataset, axis=0)
-    return (dataset - mu)/sigma
-
-scores = pd.read_csv(os.path.join(DATASET_DIR, 'scores.csv'))
-
-scores['afftype'].fillna(0, inplace=True)
-
-scores.loc[scores['melanch'] == 2, 'melanch'] = 0
-
-scores['inpatient'] %= 2 # 1 = inpatient; 0 = not
-scores['marriage'] %= 2 # 1 = married; 0 = not
-scores['work'] %= 2 # 1 = working; 0 = not
-
-scores['gender'] -= 1 # 0: male; 1: female
-
-scores['age'] = scores['age'].apply(average_str)
-scores['edu'] = scores['edu'].apply(average_str)
-
-segments = []
-labels = []
+from setup_1d_conv import *
 
 N_FEATURES = 1
-
 SEG_LEN = 4*60
-step = 60
-
-for person in scores['number']:
-    p = scores[scores['number'] == person]
-    filepath = os.path.join(DATASET_DIR, person.split('_')[0], f'{person}.csv')
-    df_activity = pd.read_csv(filepath)
-
-    for i in range(0, len(df_activity) - SEG_LEN, step):
-        segment = df_activity['activity'].values[i : i + SEG_LEN]
-        
-        if is_daytime(df_activity['timestamp'].values[i]):
-            segments.append([segment])
-            
-            if p['afftype'].values[0] == 0:
-                labels.append(0)
-            else:
-                labels.append(1)
-
-print('Segments:', len(segments))
-
-labels = to_categorical(np.asarray(labels), 2)
-segments = np.asarray(segments).reshape(-1, SEG_LEN, N_FEATURES)
-
-num_time_periods, num_sensors = segments.shape[1], segments.shape[2]
-num_classes = 2
-input_shape = num_time_periods * num_sensors
-
-segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
-labels = labels.astype('float32')
-
-X_train, X_test, y_train, y_test = train_test_split(segments, labels, test_size=0.2)
-
-""" Start machine learning """ 
-
-K.clear_session()
-
-model = Sequential()
-model.add(Reshape((SEG_LEN, num_sensors), input_shape=(input_shape,)))
-model.add(Conv1D(100, 10, activation='relu', input_shape=(SEG_LEN, num_sensors)))
-model.add(Conv1D(100, 10, activation='relu'))
-model.add(MaxPooling1D(2))
-model.add(Conv1D(160, 10, activation='relu'))
-model.add(Conv1D(160, 10, activation='relu'))
-model.add(GlobalAveragePooling1D())
-model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax'))
-
-print(model.summary())
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+STEP = 60
 
 BATCH_SIZE = 50
 EPOCHS = 100
 
-history = model.fit(X_train,
-                    y_train,
-                    batch_size=BATCH_SIZE,
-                    epochs=EPOCHS,
-                    callbacks=[
-                        EarlyStopping(monitor='val_loss', patience=3),
-                        # ModelCheckpoint(filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5', monitor='val_loss', save_best_only=True),
-                        # TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None)
-                    ],
-                    validation_split=0.2,
-                    verbose=1)
+segments, labels, num_sensors, input_shape = create_segments_and_labels(n_features, SEG_LEN, STEP, filter_timestamp='day')
+X_train, X_test, y_train, y_test = train_test_split(segments, labels, test_size=0.2)
+
+model = create_model(SEG_LEN, num_sensors, input_shape)
+
+callbacks = [
+    EarlyStopping(monitor='val_loss', patience=3),
+]
+
+history = train(model, X_train, y_train, BATCH_SIZE, EPOCHS, callbacks)
 
 y_pred_test = model.predict(X_test)
 max_y_pred_test = np.argmax(y_pred_test, axis=1)
---
+++ code/python/setup_1d_conv.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import os
+import sys
+import random
+import time
+import datetime
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+from IPython.display import display
+
+from matplotlib import pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Reshape, GlobalAveragePooling1D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+
+pd.options.mode.chained_assignment = None
+
+PROJECT_DIR = '../'
+DATASET_DIR = 'datasets'
+SAVE_DIR = 'data'
+
+try:
+    os.chdir(PROJECT_DIR)
+except:
+    print('No such directory')
+    pass
+
+verbose = len(sys.argv) > 1 and sys.argv[1] == '-v'
+
+if verbose:
+    verbose = 1
+    print('Verbose mode.')
+else:
+    verbose = 0
+
+CATEGORIES = ['CONDITION', 'CONTROL']
+LABELS = ['normal', 'bipolar']
+
+segments = []
+labels = []
+
+def make_confusion_matrix(validations, predictions, print_stdout=False, save=True):
+    matrix = confusion_matrix(validations, predictions)
+    plt.figure(figsize=(6, 4))
+    sns.heatmap(matrix,
+                cmap="coolwarm",
+                linecolor='white',
+                linewidths=1,
+                xticklabels=LABELS,
+                yticklabels=LABELS,
+                annot=True,
+                fmt="d")
+    plt.title("Confusion Matrix")
+    plt.ylabel("True Label")
+    plt.xlabel("Predicted Label")
+
+    if save:
+        plt.savefig('img/4h/confusion_matrix.png')
+
+    if print_stdout:
+        print('Confusion matrix:\n', matrix)
+
+def average_str(string):
+    if(type(string) == str):
+        num1, num2 = string.split('-')
+        num1 = int(num1)
+        num2 = int(num2)
+
+        return (num1 + num2) // 2
+
+    else:
+        return string
+
+def time_between(now, start, end):
+    if start <= end:
+        return start <= now < end
+    else:
+        return start <= now or now < end
+
+def extract_time_of_day(timestamp):
+    ts = time.strptime(timestamp.split(' ')[1], '%H:%M:%S')
+
+    for label in part_of_days.keys():
+        times = part_of_days[label]
+        start = time.strptime(times[0], '%H:%M:%S')
+        end = time.strptime(times[1], '%H:%M:%S')
+
+        if time_between(ts, start, end):
+            return label
+
+    return None
+
+def feature_normalize(dataset):
+    mu = np.mean(dataset, axis=0)
+    sigma = np.std(dataset, axis=0)
+    return (dataset - mu)/sigma
+
+def is_at_night(timestamp):
+    return time_between(time.strptime(timestamp.split(' ')[1], '%H:%M:%S'),
+                        time.strptime('22:00:00', '%H:%M:%S'),
+                        time.strptime('06:00:00', '%H:%M:%S'))
+
+def is_daytime(timestamp):
+    return time_between(time.strptime(timestamp.split(' ')[1], '%H:%M:%S'),
+                        time.strptime('06:00:00', '%H:%M:%S'),
+                        time.strptime('21:00:00', '%H:%M:%S'))
+
+def create_segments_and_labels(n_features, segment_length, step, filter_timestamp=None):
+    scores = pd.read_csv(os.path.join(DATASET_DIR, 'scores.csv'))
+    scores['afftype'].fillna(0, inplace=True)
+
+    for person in scores['number']:
+        p = scores[scores['number'] == person]
+        filepath = os.path.join(DATASET_DIR, person.split('_')[0], f'{person}.csv')
+        df_activity = pd.read_csv(filepath)
+
+        for i in range(0, len(df_activity) - segment_length, step):
+            segment = df_activity['activity'].values[i : i + segment_length]
+
+            append = False
+
+            if not filter_timestamp:
+                append = True
+
+            elif filter_timestamp == 'day' and is_daytime(df_activity['timestamp'].values[i]):
+                append = True
+                
+            elif filter_timestamp == 'night' and is_at_night(df_activity['timestamp'].values[i]):
+                append = True
+            
+            if append:
+                segments.append([segment])
+
+                if p['afftype'].values[0] == 0:
+                    labels.append(0)
+                else:
+                    labels.append(1)
+
+    labels = to_categorical(np.asarray(labels), 2)
+    segments = np.asarray(segments).reshape(-1, segment_length, n_features)
+
+    num_time_periods, num_sensors = segments.shape[1], segments.shape[2]
+    input_shape = num_time_periods * num_sensors
+
+    segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
+    labels = labels.astype('float32')
+
+    if verbose:
+        print('Segments:', len(segments))
+    
+    return segments, labels, num_sensors, input_shape
+
+def create_model(segment_length, num_sensors, input_shape, loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']):
+    K.clear_session()
+
+    model = Sequential()
+    model.add(Reshape((segment_length, num_sensors), input_shape=(input_shape,)))
+    model.add(Conv1D(100, 10, activation='relu', input_shape=(segment_length, num_sensors)))
+    model.add(Conv1D(100, 10, activation='relu'))
+    model.add(MaxPooling1D(2))
+    model.add(Conv1D(160, 10, activation='relu'))
+    model.add(Conv1D(160, 10, activation='relu'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+
+    if verbose:
+        print(model.summary())
+
+    return model
+
+def train(model, X_train, y_train, batch_size, epochs, callbacks, validation_split=0.2):
+    return model.fit(X_train,
+                    y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    callbacks=callbacks,
+                    validation_split=validation_split,
+                    verbose=verbose)
+
+def predict(model, X_test, y_test):
+    y_pred_test = model.predict(X_test)
+    max_y_pred_test = np.argmax(y_pred_test, axis=1)
+    max_y_test = np.argmax(y_test, axis=1)
+
+    print(classification_report(max_y_test, max_y_pred_test))
+
+    return max_y_test, max_y_pred_test

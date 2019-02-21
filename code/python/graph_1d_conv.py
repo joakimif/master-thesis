ec7644091add9247b310fb041b72341a59ffee2e
Joakim Frogner <joakim@inligo.no>
2019-02-21 12:18:32 +0100

    code

--- code/python/graph_1d_conv.py
+++ code/python/graph_1d_conv.py
@@ -6,11 +6,14 @@ if not model_path:
     print('Usage: python3 graph_1d_conv.py [options] --model_path <path_to_models>')
     exit()
 
+results = []
+
 for f in os.listdir(model_path):
     if '.h5' in f and 'Conv1D' in f:
         print('\n=============')
         print(f'Loading model: {f}')
 
+        f = f.replace('.h5', '')
         t, ts, seg, step, epochs, batch = f.split('_')
 
         segments, labels, num_sensors, input_shape = create_segments_and_labels(N_FEATURES, int(seg), int(step))
@@ -20,10 +23,13 @@ for f in os.listdir(model_path):
         X_train, X_test, y_train, y_test = train_test_split(segments, labels, test_size=0.2)
 
         loss, acc = model.evaluate(X_test, y_test)
-
-        print('Accuracy: {:5.2f}%'.format(100 * acc))
-        print('Loss: {:5.2f}%'.format(100 * loss))
-
         max_y_test, max_y_pred_test = predict(model, X_test, y_test)
 
-        make_confusion_matrix(max_y_test, max_y_pred_test, print_stdout=True)
+        results.append((f, loss, acc, max_y_test, max_y_pred_test))
+
+        # print('Accuracy: {:5.2f}%'.format(100 * acc))
+        # print('Loss: {:5.2f}%'.format(100 * loss))
+        # make_confusion_matrix(max_y_test, max_y_pred_test, print_stdout=True)
+
+for result in results:
+    print(result)

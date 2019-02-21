3051e00ca33b6fe7f350f27feabc7288bc60588e
Joakim Frogner <joakim@inligo.no>
2019-02-21 13:18:31 +0100

    code

--- code/python/graph_1d_conv.py
+++ code/python/graph_1d_conv.py
@@ -11,6 +11,7 @@ if not model_path:
     exit()
 
 histories = []
+seg_lengths = []
 
 for f in reversed(os.listdir(model_path)):
     if '.h5' in f and 'Conv1D' in f:
@@ -28,12 +29,28 @@ for f in reversed(os.listdir(model_path)):
         h = train(model, X_train, y_train, batch_size, epochs, callbacks, validation_split=0.4)
 
         df = pd.DataFrame(h.history, index=h.epoch)
+        
         histories.append(df)
-
-        df.plot()
+        seg_lengths.append(seg)
 
         if len(histories) > 1:
             break
 
+historydf = pd.concat(dflist, axis=1)
+
+metrics_reported = dflist[0].columns
+historydf.columns = pd.MultiIndex.from_product([seg_lengths, metrics_reported], names=['segment lengths', 'metric'])
+
+ax = plt.subplot(211)
+historydf.xs('loss', axis=1, level='metric').plot(ylim=(0,1), ax=ax)
+plt.title("Loss")
+
+ax = plt.subplot(212)
+historydf.xs('acc', axis=1, level='metric').plot(ylim=(0,1), ax=ax)
+plt.title('Accuracy')
+
+plt.xlabel('Epochs')
+
+plt.tight_layout()
 plt.savefig('../img/plot.png')
 

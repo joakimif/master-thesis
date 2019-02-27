08fdd93fbb4b4547291e1b08d02fac108ac7caa0
Joakim Frogner <joakim@inligo.no>
2019-02-27 13:06:51 +0100

    clean file

---
+++ code/python/madrs_score_prediction.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import os
+import sys
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+from matplotlib import pyplot as plt
+
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Input, Dense, Dropout, Flatten, Reshape, GlobalAveragePooling1D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+
+DATASET_DIR = '../datasets'
+scores = pd.read_csv(os.path.join(DATASET_DIR, '/scores.csv'))
+scores['madrs2'].fillna(0, inplace=True)
+
+""" Create segments and labels """
+
+segments = []
+labels = []
+
+for person in scores['number']:
+    p = scores[scores['number'] == person]
+    filepath = os.path.join(DATASET_DIR, person.split('_')[0], f'{person}.csv') # ../datasets/[control or condition]/*.csv
+    df_activity = pd.read_csv(filepath)
+
+    for i in range(0, len(df_activity) - segment_length, step):
+        segment = df_activity['activity'].values[i : i + segment_length]
+        
+        segments.append([segment])
+        labels.append(p['madrs2'].values[0])
+
+segments = np.asarray(segments)
+segments = segments.reshape(-1, segment_length, n_features)
+
+input_shape = segments.shape[1]
+segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
+labels = np.asarray(labels).astype('float32')
+
+X_train, X_test, y_train, y_test = train_test_split(segments, labels, test_size=0.2)
+
+""" Create model """
+
+model = Sequential()
+model.add(Reshape((segment_length, 1), input_shape=(input_shape,)))
+model.add(Conv1D(100, 10, activation='relu', input_shape=(segment_length, 1)))
+model.add(Conv1D(100, 10, activation='relu'))
+model.add(MaxPooling1D(2))
+model.add(Conv1D(160, 10, activation='relu'))
+model.add(Conv1D(160, 10, activation='relu'))
+model.add(GlobalAveragePooling1D())
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_logarithmic_error', optimizer='nadam', metrics=['mse'])
+
+""" Train model """
+
+h = model.fit(X_train,
+                y_train,
+                batch_size=100,
+                epochs=40,
+                callbacks=[
+                    EarlyStopping(monitor='mean_squared_error', patience=2),
+                ],
+                validation_split=0.2,
+                verbose=1)
+
+print(model.evaluate(X_test, y_test))
+

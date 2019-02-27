cd41ff20964cd3acadb6e8b9f74b3040169f266d
Joakim Frogner <joakim@inligo.no>
2019-02-27 16:29:53 +0100

    code

--- code/python/1d_conv.py
+++ code/python/1d_conv.py
@@ -26,7 +26,7 @@ if k_folds > 1:
 
         model = create_model(segment_length, num_sensors, input_shape, output_classes=output_classes, dropout=dropout, verbose=0)
         history = train(model, X_train, y_train, batch_size, epochs, callbacks=[EarlyStopping(monitor='val_acc', patience=2)], validation_split=0.4, verbose=0)
-        loss, acc = evaluate(model, X_test, y_test, verbose=0)
+        loss, acc = evaluate(model, X_test, y_test, verbose=0) 
 
         if i == 0 or max_acc < acc:
             print(f'New best accuracy: {acc}')

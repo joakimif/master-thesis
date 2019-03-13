40d41810b79507fc2d9449cbcb0e612232df592e
Joakim Frogner <joakim@inligo.no>
2019-03-13 12:09:24 +0100

    code

--- code/python/regression.py
+++ code/python/regression.py
@@ -25,7 +25,7 @@ from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 def regression_model():
 	model = Sequential()
 	model.add(Dense(7, input_dim=7, activation='relu'))
-	model.add(Dense(1))
+	model.add(Dense(1, activation='sigmoid'))
 
 	model.compile(loss='mean_squared_error', optimizer='adam')
 	return model

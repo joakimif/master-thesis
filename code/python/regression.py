e0e53fe64beedd45e0822a41c481896f48415a79
Joakim Frogner <joakim@inligo.no>
2019-03-15 12:13:59 +0100

    code

--- code/python/regression.py
+++ code/python/regression.py
@@ -29,7 +29,6 @@ def regression_model():
     model.add(Dense(1))
 
     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
-    print(model.summary())
     return model
 
 
@@ -76,6 +75,7 @@ for X_col in X_columns:
         regressor = load_model(f'../models/kerasregressor_{X_col}.h5')
     else:
         regressor = KerasRegressor(build_fn=regression_model, epochs=epochs, batch_size=batch_size, verbose=1)
+        print(regressor.model.summary())
         exit()
         h = regressor.fit(X_train, y_train)
         regressor.model.save(f'../models/kerasregressor_{X_col}.h5')

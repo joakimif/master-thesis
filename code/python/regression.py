58a8e9ed869fed3160512b4795ffa992ca963cc9
Joakim Frogner <joakim@inligo.no>
2019-03-15 12:14:34 +0100

    code

--- code/python/regression.py
+++ code/python/regression.py
@@ -75,7 +75,7 @@ for X_col in X_columns:
         regressor = load_model(f'../models/kerasregressor_{X_col}.h5')
     else:
         regressor = KerasRegressor(build_fn=regression_model, epochs=epochs, batch_size=batch_size, verbose=1)
-        print(regressor.model.summary())
+        print(regressor.summary())
         exit()
         h = regressor.fit(X_train, y_train)
         regressor.model.save(f'../models/kerasregressor_{X_col}.h5')

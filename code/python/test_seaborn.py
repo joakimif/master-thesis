d6bf8d148522244806c01744e89bfbd0eafbffd0
Joakim Frogner <joakim@inligo.no>
2019-03-21 16:44:40 +0100

    code

--- code/python/test_seaborn.py
+++ code/python/test_seaborn.py
@@ -1,11 +1,11 @@
 import seaborn as sns
 
 def heatmap():
-	true_positive = 1337
-	false_positive = 200
-	true_negative = 777
-	false_negative = 99
-	
+    true_positive = 1337
+    false_positive = 200
+    true_negative = 777
+    false_negative = 99
+    
     matrix = np.array([[true_positive, false_negative], [false_positive, true_negative]])
     plt.figure(figsize=(6, 4))
     sns.heatmap(matrix,

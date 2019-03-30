f63850ee8b5e37b0428696f8f96300ebbfde699b
Joakim Frogner <joakim@inligo.no>
2019-03-30 14:55:48 +0100

    code

---
+++ code/python/cnn/utils.py
@@ -0,0 +1,32 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+
+def create_segments_and_labels_prediction(scores_file):
+    scores = pd.read_csv(scores_file)
+    scores['madrs2'].fillna(0, inplace=True)
+
+    segments = []
+    labels = []
+
+    for person in scores['number']:
+        p = scores[scores['number'] == person]
+        filepath = os.path.join(DATASET_DIR, person.split('_')[0], f'{person}.csv')
+        df_activity = pd.read_csv(filepath)
+
+        for i in range(0, len(df_activity) - segment_length, step):
+            segment = df_activity['activity'].values[i : i + segment_length]
+            
+            segments.append([segment])
+            labels.append(p['madrs2'].values[0])
+
+    segments = np.asarray(segments)
+    segments = segments.reshape(-1, segment_length, 1)
+
+    input_shape = segments.shape[1]
+    segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
+    labels = np.asarray(labels).astype('float32')
+
+    return segments, labels, input_shape

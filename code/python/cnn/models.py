8e3e8d20edd26fe50d4c9f39b0abdeae224ba997
Joakim Frogner <joakim@inligo.no>
2019-03-30 17:06:30 +0100

    code

--- code/python/cnn/models.py
+++ code/python/cnn/models.py
@@ -249,7 +249,7 @@ class PredictionModel(Conv1DModel):
         self.compile()
 
 
-    def graph_history(self, metric='val_loss', title='Training Loss', xlabel='Epoch', ylabel='Mean Squared Error'):
+    def graph_history(self, metric='loss', title='Training Loss', xlabel='Epoch', ylabel='Mean Squared Error'):
         assert self.history != None and self.history.history != None, 'Model must be fit before generating history graph.'
 
         plt.clf()

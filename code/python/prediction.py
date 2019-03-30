eddc4b28020857b3badbe84ec78ef27018c6c3a4
Joakim Frogner <joakim@inligo.no>
2019-03-30 14:35:04 +0100

    code

---
+++ code/python/prediction.py
@@ -0,0 +1,10 @@
+from cnn.models import PredictionModel
+
+step = 60
+segment_length = 2880
+learning_rate = 0.0001
+optimizer = 'adam'
+verbose = 1
+
+model = PredictionModel(segment_length=segment_length, step=step, learning_rate=learning_rate, optimizer=optimizer, verbose=verbose)
+

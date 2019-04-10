a22afa415a6a297b5eea6da50e6c0c171d228411
Joakim Frogner <joakim@inligo.no>
2019-04-10 11:14:20 +0200

    leave one out experiment

--- code/python/cnn/utils.py
+++ code/python/cnn/utils.py
@@ -1,8 +1,85 @@
 import os
+import random
 
 import numpy as np
 import pandas as pd
 
+def create_segments_and_labels_loo(dataset_dir, segment_length, step):
+    scores = pd.read_csv(os.path.join(dataset_dir, 'scores.csv'))
+    scores['afftype'].fillna(0, inplace=True)
+    
+    segments = []
+    labels = []
+
+    left_out_segments = []
+    left_out_labels = []
+    left_out_id = None
+
+    r = random.randint(0, len(scores['number']))
+
+    for i, person in enumerate(scores['number']):
+        p = scores[scores['number'] == person]
+        filepath = os.path.join(dataset_dir, person.split('_')[0], f'{person}.csv')
+        df_activity = pd.read_csv(filepath)
+
+        if i == r:
+            left_out_id = p['number']
+
+        for j in range(0, len(df_activity) - segment_length, step):
+            segment = df_activity['activity'].values[j : j + segment_length]
+            
+            if i == r:
+                left_out_segments.append([segment])
+                left_out_labels.append(p['afftype'].values[0] == 0 and 0 or 1)
+            else:
+                segments.append([segment])
+                labels.append(p['afftype'].values[0] == 0 and 0 or 1)
+
+    labels = np.asarray(labels).astype('float32')
+    left_out_labels = np.asarray(left_out_labels).astype('float32')
+
+    segments = np.asarray(segments).reshape(-1, segment_length, 1)
+    left_out_segments = np.asarray(left_out_segments).reshape(-1, segment_length, 1)
+
+    num_time_periods, num_sensors = segments.shape[1], segments.shape[2]
+    input_shape = num_time_periods * num_sensors
+
+    segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
+    
+    return segments, labels, left_out_segments, left_out_labels, input_shape
+
+
+def create_segments_and_labels(dataset_dir, segment_length, step):
+    scores = pd.read_csv(os.path.join(dataset_dir, 'scores.csv'))
+    scores['afftype'].fillna(0, inplace=True)
+    
+    segments = []
+    labels = []
+
+    for person in scores['number']:
+        p = scores[scores['number'] == person]
+        filepath = os.path.join(dataset_dir, person.split('_')[0], f'{person}.csv')
+        df_activity = pd.read_csv(filepath)
+
+        for i in range(0, len(df_activity) - segment_length, step):
+            segment = df_activity['activity'].values[i : i + segment_length]
+            segments.append([segment])
+
+            if p['afftype'].values[0] == 0:
+                labels.append(0)
+            else:
+                labels.append(1)
+
+    labels = np.asarray(labels).astype('float32')
+    
+    segments = np.asarray(segments).reshape(-1, segment_length, 1)
+
+    num_time_periods, num_sensors = segments.shape[1], segments.shape[2]
+    input_shape = num_time_periods * num_sensors
+
+    segments = segments.reshape(segments.shape[0], input_shape).astype('float32')
+    
+    return segments, labels, input_shape
 
 def create_segments_and_labels_prediction(dataset_dir, segment_length, step):
     scores = pd.read_csv(os.path.join(dataset_dir, 'scores.csv'))
---
+++ code/python/leave_one_out.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import random
+
+import numpy as np
+import pandas as pd
+
+#from cnn.models import ClassificationModel
+from cnn.utils import create_segments_and_labels_loo
+
+from parse_args import *
+
+DATASET_DIR = '../datasets'
+
+segments, labels, left_out_segments, left_out_labels, input_shape = create_segments_and_labels_loo(DATASET_DIR, segment_length, step)
+
+model = ClassificationModel(input_shape=input_shape, segment_length=segment_length, step=step, learning_rate=learning_rate, optimizer=optimizer, verbose=verbose, dropout=0.5)
+
+model.fit(X_train, y_train, batch_size, epochs, validation_split=0.4)
+# model.graph_history(filetype='pdf')
+
+print(model.predict(left_out_segments))

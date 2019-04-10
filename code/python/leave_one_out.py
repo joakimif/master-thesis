ce618a7c7120bde71a65e4df7c81b615028564d0
Joakim Frogner <joakim@inligo.no>
2019-04-10 12:08:22 +0200

    leave one out experiment

--- code/python/leave_one_out.py
+++ code/python/leave_one_out.py
@@ -16,13 +16,13 @@ DATASET_DIR = '../datasets'
 
 print('Participant,Correct,Prediction,Votes,Total')
 
-model = ClassificationModel(input_shape=input_shape, segment_length=segment_length, step=step, optimizer=optimizer, verbose=0, dropout=0.5, n_output_classes=2)
-
 for i in range(0, 56):
     K.clear_session()
-
+    model = None
+    
     segments, labels, left_out_segments, left_out_group, input_shape = create_segments_and_labels_loo(DATASET_DIR, segment_length, step, leave_out_id=i)
 
+    model = ClassificationModel(input_shape=input_shape, segment_length=segment_length, step=step, optimizer=optimizer, verbose=0, dropout=0.5, n_output_classes=2)
     model.fit(segments, labels, batch_size, epochs)
     prediction = model.majority_voting_prediction(left_out_segments)
 
